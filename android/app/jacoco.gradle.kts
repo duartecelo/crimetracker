f98e26f829e8b6577cf7512d6d62f0b2ec2bc9a2
Guilherme Perlasca <perlasca47@gmail.com>
2025-11-25 17:59:21 -0300

    feat: Adição sonar

---
+++ android/app/jacoco.gradle.kts
@@ -0,0 +1,55 @@
+import org.gradle.api.tasks.testing.Test
+import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
+tasks.withType<Test> {
+    configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+}
+
+val excludes = listOf(
+    "**/R.class",
+    "**/R$*.class",
+    "**/BuildConfig.*",
+    "**/Manifest*.*",
+    "**/*Test*.*",
+    "android/**/*.*",
+    "**/*MapperImpl*.*",
+    "**/*\$ViewInjector*.*",
+    "**/*\$ViewBinder*.*",
+    "**/BuildConfig.*",
+    "**/*Component*.*",
+    "**/*BR*.*",
+    "**/Manifest*.*",
+    "**/*\$Lambda$*.*",
+    "**/*Companion*.*",
+    "**/*Module*.*",
+    "**/*Dagger*.*",
+    "**/*Hilt*.*",
+    "**/*MembersInjector*.*",
+    "**/*_MembersInjector.class",
+    "**/*_Factory*.*",
+    "**/*_Provide*Factory*.*",
+    "**/*Extensions*.*"
+)
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDebugUnitTest")
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+
+    val debugTree = fileTree("${project.layout.buildDirectory.get()}/tmp/kotlin-classes/debug") {
+        exclude(excludes)
+    }
+    val mainSrc = "${project.projectDir}/src/main/java"
+
+    sourceDirectories.setFrom(files(mainSrc))
+    classDirectories.setFrom(files(debugTree))
+    executionData.setFrom(fileTree(project.layout.buildDirectory.get()) {
+        include("outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec")
+    })
+}
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,8 +5,18 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.52" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("org.sonarqube") version "6.0.1.5171"
 }
+
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "marceloduarte_crimetracker_android")
+        property("sonar.organization", "marceloduarte")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
